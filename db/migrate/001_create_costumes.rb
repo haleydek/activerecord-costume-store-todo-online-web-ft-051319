d1ad159d583e2d9f2fda3132abf28a8e62cf40cc
Haley DeKeyser <hcd09@live.com>
2019-06-18 17:29:50 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,7 +1,11 @@
 class CreateCostumes << ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
-      t.
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.string :size
+      
   
 end
 
