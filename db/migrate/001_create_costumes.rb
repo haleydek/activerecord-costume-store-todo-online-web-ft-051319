374129053bcd04e8cda6d42be81bf29cf884141c
Haley DeKeyser <hcd09@live.com>
2019-06-18 17:31:47 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,4 +1,5 @@
 class CreateCostumes << ActiveRecord::Migration[4.2]
+  
   def change
     create_table :costumes do |t|
       t.string :name
@@ -6,16 +7,6 @@ class CreateCostumes << ActiveRecord::Migration[4.2]
       t.string :image_url
       t.string :size
       t.timestamps
-      
+    end
   
 end
-
-# Create a class and inherit from ActiveRecord::Migration
-
-# By convention, the class name should match the part of the
-# file name after the number, so in this case:
-# 002_create_costumes.rb becomes class CreateCostumes
-
-# Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
-# value types according to the spec
