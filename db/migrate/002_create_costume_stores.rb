6a0e6de8dfe5bb3aca925f41d38625ffdf8d217a
Haley DeKeyser <hcd09@live.com>
2019-06-18 17:38:21 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,7 +1,7 @@
-class CostumeStore < ActiveRecord::Migration[4.2]
+class CostumeStores < ActiveRecord::Migration[4.2]
   
   def change
-    create_table :costume_stores do |t|
+    create_table :costume_store do |t|
       t.string :name
       t.string :location
       t.integer :costume_inventory
